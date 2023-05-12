4b2f22039a0d68a150befd3cb4bc978221f213f3
Hyeseon Lee <hyesun1999@gmail.com>
2023-05-12 15:07:45 +0900

    #3 id를 state로 따로  관리하기로 결정함, checked 상태 바꾸기(+checked 상태에 따라 text-decoration 설정함), delete 기능도 만듦 !

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,16 +2,10 @@ import React, { useState } from "react";
 
 export default function Board() {
   // ui의 변화가 있는 것들은 다 state로 관리한다.
-  let nextId = 0;
 
-  function addId() {
-    console.log("ejgo ");
-    nextId++;
-    console.log(nextId);
-  }
   const [items, setItems] = useState([]);
-
   const [inputValue, setInputValue] = useState("");
+  const [nextId, setNextId] = useState(1);
 
   function handleTodoTextChange(e) {
     console.log(e.target.value);
@@ -29,28 +23,62 @@ export default function Board() {
       day: "numeric",
       weekday: "long",
     });
-    setInputValue("");
     console.log("iv", inputValue);
     setItems((items) => [
       ...items,
-      { id: addId(), date: date, text: inputValue, completed: false },
+      {
+        id: nextId,
+        date: date,
+        text: inputValue,
+        completed: false,
+      },
     ]);
+    setNextId((nextId) => nextId + 1);
+    setInputValue("");
 
     console.log("최종 제출 item", items);
   }
 
+  function handleChecked(itemId) {
+    // items에서 itemId와 일치한 것을 찾고,
+    // check상태 바꾸기
+
+    setItems((items) =>
+      items.map((item) =>
+        item.id === itemId ? { ...item, completed: !item.completed } : item
+      )
+    );
+  }
+  function handleDeleted(itemId) {
+    setItems((items) => items.filter((item) => item.id !== itemId));
+  }
   return (
     <>
-      <div className="w-60 h-72 bg-pink-400">
+      <div className="w-96 h-96 bg-pink-400">
         <div>카테고리</div>
-        <div>
+        <div className="p-3">
           {items.map((item) => (
-            <>
-              <p>{item.id}</p>
+            <div key={item.id} className="flex items-center">
+              <div
+                className="w-3 h-3 aspect-squared bg-white mr-2 flex justify-center items-center p-2"
+                onClick={() => handleChecked(item.id)}
+              >
+                {item.completed ? "v" : undefined}
+              </div>
+              {item.completed ? (
+                <p className="line-through">{item.text}</p>
+              ) : (
+                <p>{item.text}</p>
+              )}
               <p>{item.date}</p>
-              <p>{item.text}</p>
-              <p>{item.completed}</p>
-            </>
+
+              <div
+                className="w-3 h-3 aspect-squared bg-white mr-2 flex justify-center items-center p-2"
+                onClick={() => handleDeleted(item.id)}
+              >
+                T
+              </div>
+            </div>
           ))}
         </div>
         <div>
